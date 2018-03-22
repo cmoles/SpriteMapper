40648864ebc8471f6497bac6aaa64992fc3feca1
Chris Moles <cmoles@gmail.com>
2018-03-21 23:37:26 -0700

    add sprite class

---
+++ js/sprite.js
@@ -0,0 +1,20 @@
+var Sprite = function() {
+  return this.init(arguments);
+}
+
+Sprite.prototype.x = 0;
+Sprite.prototype.y = 0;
+Sprite.prototype.w = 0;
+Sprite.prototype.h = 0;
+
+Sprite.prototype.init = function (args) {
+  this.x = args[0] || 0;
+  this.y = args[1] || 0;
+  this.w = args[2] || 0;
+  this.h = args[3] || 0;
+  return this;
+}
+
+Sprite.prototype.toList = function () {
+  return [this.x, this.y, this.w, this.h];
+}
